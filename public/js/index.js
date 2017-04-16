46d90e71b535294c879ba9081e9735cb2b38d87a
Fernando Nantes-Machado <fernandonantesmachadoytoledo@gmail.com>
2017-04-16 16:24:40 +0100

    Separadas las clases medida y temperaturas en ficheros distintos

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 $('#temp-form').on('submit', function(e){
     e.preventDefault();
     $('#result').text(Medida.convertir(temperatura.value));
--- public/js/medida.js
+++ public/js/medida.js
@@ -31,58 +31,6 @@ class Medida {
 
 }
 
-
-class Temperatura  extends Medida {
-    constructor(val, type){
-
-        super(parseFloat(val), type);
-    }
-    toCelsius(){
-        return this;
-    }
-    toFahrenheit(){
-        return this;
-    }
-    toKelvin(){
-        return this;
-    }
-}
-class Celsius extends Temperatura {
-    constructor(val){
-        super(val, 'Celsius')
-    }
-    toFahrenheit(){
-        return new Fahrenheit(this.value * 9/5)+32;
-
-    }
-    toKelvin(){
-        return new Kelvin(this.value + 273.15);
-    }
-}
-class Fahrenheit extends Temperatura {
-    constructor(val){
-        super(val, 'Fahrenheit')
-    }
-    toKelvin(){
-        return new Kelvin((this.value + 459.67) * 5/9);
-    }
-    toCelsius(){
-        return new Celsius((this.value -32 )*5/9);
-    }
-}
-class Kelvin extends Temperatura {
-
-    constructor(val){
-        super(val, 'Kelvin')
-    }
-    toCelsius(){
-        return new Celsius(this.value - 273.15);
-    }
-    toFahrenheit(){
-        return new Fahrenheit((this.value * 9/5)- 459.67);
-    }
-}
-
 (function(){
     measures = {
         'K': {
